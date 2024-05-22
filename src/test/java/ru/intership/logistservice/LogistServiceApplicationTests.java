ad7b087e19388ba1dc83f516868a0bc26ddcb25c
Viktor Dyukov <snailrake@mail.ru>
2024-05-22 16:24:53 +0400

    Initial commit

---
+++ src/test/java/ru/intership/logistservice/LogistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intership.logistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogistServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
