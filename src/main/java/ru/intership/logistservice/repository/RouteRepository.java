50f2159b0e67c7f74137bc11ec38b078b976150d
Viktor Dyukov <snailrake@mail.ru>
2024-06-03 17:03:06 +0400

    refactor: minor changes

--- src/main/java/ru/intership/logistservice/repository/RouteRepository.java
+++ src/main/java/ru/intership/logistservice/repository/RouteRepository.java
@@ -14,11 +14,13 @@ public interface RouteRepository extends JpaRepository<Route, Long> {
     List<Route> findAllByTaskId(long taskId, Pageable pageable);
 
     @Query(nativeQuery = true, value = """
-            SELECT COUNT(CASE WHEN route_event.event_type = :eventType AND DATE(route_event.occurred_at) = CURRENT_DATE THEN 1 END)
-            FROM route_event
-                JOIN route ON route_event.route_id = route.id
-                JOIN task ON route.task_id = task.id
-            WHERE task.company_id = :companyId
+            SELECT COUNT(CASE WHEN event_type = :eventType AND DATE(occurred_at) = CURRENT_DATE THEN 1 END)
+            FROM (SELECT route_event.*,
+                  ROW_NUMBER() OVER (PARTITION BY route_id ORDER BY occurred_at DESC) AS rn
+                  FROM route_event) AS re
+                     JOIN route ON re.route_id = route.id
+                     JOIN task ON route.task_id = task.id
+            WHERE task.company_id = :companyId AND re.rn = 1;
             """)
     long findDailyEventCountByCompanyId(String eventType, String companyId);
 }
